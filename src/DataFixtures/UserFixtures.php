6971040cfdfdb00d322e7556fe8dbdb55fb7ed73
zimzim62000 <zimzim62000@gmail.com>
2019-12-09 10:00:16 +0100

    add base project

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class UserFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('ZimZim');
+        $plainPassword = $user->getUsername();
+        $encoded = $this->encoder->encodePassword($user, $plainPassword);
+        $user->setPassword($encoded);
+        $user->addRole('ROLE_ADMIN');
+        //$user->setCreatedAt(new \DateTime('now'));
+        $this->addReference($user->getUsername(), $user);
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('Fred');
+        $plainPassword = $user->getUsername();
+        $encoded = $this->encoder->encodePassword($user, $plainPassword);
+        $user->setPassword($encoded);
+        //$user->setCreatedAt(new \DateTime('now'));
+        $this->addReference($user->getUsername(), $user);
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setUsername('Giles');
+        $plainPassword = $user->getUsername();
+        $encoded = $this->encoder->encodePassword($user, $plainPassword);
+        $user->setPassword($encoded);
+        //$user->setCreatedAt(new \DateTime('now'));
+        $this->addReference($user->getUsername(), $user);
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
