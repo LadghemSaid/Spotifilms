7cfc2b267a687be62c02b118684b54cb649f00f5
Said <said.ladghem@gmail.com>
2019-12-19 16:53:43 +0100

    Integration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SeriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(SeriesRepository $series_Repo)
     {
-        return $this->render('home/index.html.twig');
+$series =$series_Repo->findBy(array(), array('id' => 'DESC'),15);
+        return $this->render('home/index.html.twig', [
+            "series" => $series
+        ]);
     }
 }
